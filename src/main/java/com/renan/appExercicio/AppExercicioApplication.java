fbe5b43cbc8b3d7f60031f7d6072266814f8a864
Renan Carlos <renancarlosgr@hotmail.com>
2019-07-03 18:19:54 -0300

    Criação do projeto

---
+++ src/main/java/com/renan/appExercicio/AppExercicioApplication.java
@@ -0,0 +1,13 @@
+package com.renan.appExercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppExercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppExercicioApplication.class, args);
+	}
+
+}
