29b55db80b7f5dceb65bc635370aede0cf4497d7
Renan Carlos <renancarlosgr@hotmail.com>
2019-07-04 17:14:09 -0300

    Classe Categoria

--- src/main/java/com/renan/SpringApp/AppExercicioApplication.java
+++ src/main/java/com/renan/SpringApp/AppExercicioApplication.java
@@ -1,4 +1,4 @@
-package com.renan.appExercicio;
+package com.renan.SpringApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/renan/SpringApp/AppExercicioApplicationTests.java
+++ src/test/java/com/renan/SpringApp/AppExercicioApplicationTests.java
@@ -1,4 +1,4 @@
-package com.renan.appExercicio;
+package com.renan.SpringApp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
