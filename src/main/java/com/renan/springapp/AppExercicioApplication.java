2429e582014b3f2b6fb9d181f742f6233acc9e01
Renan Carlos <renancarlosgr@hotmail.com>
2019-07-11 23:50:06 -0300

    Cliente, endereço e telefone

--- src/main/java/com/renan/springapp/AppExercicioApplication.java
+++ src/main/java/com/renan/springapp/AppExercicioApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.renan.springapp.domain.Categoria;
 import com.renan.springapp.domain.Cidade;
+import com.renan.springapp.domain.Cliente;
+import com.renan.springapp.domain.Endereco;
 import com.renan.springapp.domain.Estado;
 import com.renan.springapp.domain.Produto;
+import com.renan.springapp.domain.enums.TipoCliente;
 import com.renan.springapp.repositories.CategoriaRepository;
 import com.renan.springapp.repositories.CidadeRepository;
+import com.renan.springapp.repositories.ClienteRepository;
+import com.renan.springapp.repositories.EnderecoRepository;
 import com.renan.springapp.repositories.EstadoRepository;
 import com.renan.springapp.repositories.ProdutoRepository;
 
@@ -27,7 +32,11 @@ public class AppExercicioApplication implements CommandLineRunner{
 	private EstadoRepository estRepo;
 	@Autowired
 	private CidadeRepository cidRepo;
-
+	@Autowired
+	private ClienteRepository cliRepo;
+	@Autowired
+	private EnderecoRepository endRepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AppExercicioApplication.class, args);
 	}
@@ -64,6 +73,18 @@ public class AppExercicioApplication implements CommandLineRunner{
 		
 		estRepo.saveAll(Arrays.asList(est1,est2));
 		cidRepo.saveAll(Arrays.asList(c1,c2,c3));
+		
+		Cliente cli1 = new Cliente(null,"Maria Silva", "maria@email.com","456465456",TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("4564654465","4564656"));
+		
+		Endereco e1 = new Endereco(null,"Rua FLores","300","apt 303", "Jardim", "450245646",cli1,c1);
+		Endereco e2 = new Endereco(null, "av matos","300","sala 1","Centro","456465",cli1,c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
+		cliRepo.saveAll(Arrays.asList(cli1));
+		endRepo.saveAll(Arrays.asList(e1,e2));
 		
 	}
 
